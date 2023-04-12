49cf5a28b7c787fa9dc34450e11a8831547c1b65
Thomas Haferlach <thomash@pollinations.ai>
2023-04-12 17:38:13 +0000

    changes

--- predict.py
+++ predict.py
@@ -13,17 +13,24 @@ class Predictor(BasePredictor):
             default=4),
         ) -> Path:
         print("predict")
-        #os.system("rm /tmp/*.mp4")
+        # os.system("rm /tmp/*.mp4")
         
-        out_path = f"/src/interpolated_{interpolation_factor}x.mp4"
+        out_path = f"/tmp/interpolated.mp4"
+        out_path_final = f"/tmp/interpolated_{interpolation_factor}x.mp4"
+
+        # use ffmpeg to remove duplicate frames
+        os.system(f"ffmpeg -i {str(video)} -vf mpdecimate {str(video)}_nodup.mp4")
+
+
         # os.system(f"rm -rf {out_path}")
         os.system(f"python3 inference_video.py --exp={interpolation_factor} --video=\"{str(video)}\" --output=\"{out_path}\"") 
 
         # reencode with -c:v libx264 -crf 20 -preset slow -vf format=yuv420p -c:a aac -movflags +faststart
 
-        # os.system(f"ffmpeg -i {out_path} -c:v libx264 -crf 20 -preset slow -vf format=yuv420p -c:a aac -movflags +faststart {out_path}.")
+        os.system(f"ffmpeg -i {out_path} -c:v libx264 -crf 20 -preset slow -vf format=yuv420p -c:a aac -movflags +faststart {out_path_final}")
+
         os.system("ls -l /src")
         os.system("ls -l /tmp")
 
-        return Path(out_path)
+        return Path(out_path_final)
 
